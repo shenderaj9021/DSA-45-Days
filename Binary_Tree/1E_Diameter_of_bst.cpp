b12a3a7627265d9591502f6f11a5bee49f787b97
shenderaj9021 <rajshende220802@gmail.com>
2022-06-07 23:10:24 +0530

    Bst

---
+++ Binary_Tree/1E_Diameter_of_bst.cpp
@@ -0,0 +1,37 @@
+/*https://leetcode.com/problems/diameter-of-binary-tree/
+
+
+
+
+
+
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+
+class Solution {
+public:
+    int m =0;
+    int helper(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int  l =helper(root->left);
+        int r = helper(root->right);
+        m =max(m,l+r);
+        return max(l,r)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+       int ans =helper(root);
+        return m;
+    }
+
+};
+*/
